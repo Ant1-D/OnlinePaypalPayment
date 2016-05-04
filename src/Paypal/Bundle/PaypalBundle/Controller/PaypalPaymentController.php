a0b2b7077a8fe58833549bfa10426331acfb5c29
Juicy <julien.decoen@gmail.com>
2016-05-04 18:35:06 +0200

    add route & vue

--- src/Paypal/Bundle/PaypalBundle/Controller/PaypalPaymentController.php
+++ src/Paypal/Bundle/PaypalBundle/Controller/PaypalPaymentController.php
@@ -4,18 +4,17 @@ namespace Paypal\Bundle\PaypalBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class PaypalPaymentController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="PaypalBundle")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return new Response("Hello World !");
     }
 }
